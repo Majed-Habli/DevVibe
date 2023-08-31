4c1840b93365862422f0c4fb9a7a5f12b89a11ab
User <majedhabli2020@gmail.com>
2023-08-31 23:49:20 +0300

    Server <> Created the migration schemas.

--- Back-End-Laravel/Server-DevVibe/database/migrations/2014_10_12_000000_create_users_table.php
+++ Back-End-Laravel/Server-DevVibe/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,68 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->integer('user_type');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password')->nullable();
+            $table->string('resume')->nullable();
+            $table->string('description');
+            $table->string('profile_image_url');
+            $table->boolean('has_access')->default(false);
             $table->timestamps();
         });
+
+        Schema::create('user_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->timestamps();
+        });
+
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('image_url');
+            $table->timestamps();
+        });
+
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id();
+            $table->integer('name');
+            $table->timestamps();
+        });
+
+        Schema::create('user_skills', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('skill_id');
+            $table->timestamps();
+        });
+
+        Schema::create('swipes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('swiped_user_id');
+            $table->boolean('is_liked')->default(false);
+            $table->timestamps();
+        });
+
+        Schema::create('matchs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_one_id');
+            $table->integer('user_two_id');
+            $table->timestamps();
+        });
+
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('match_id');
+            $table->integer('user_one_id');
+            $table->integer('user_two_id');
+            $table->text('message');
+            $table->timestamps();
+        });
+
     }
 
     /**
@@ -28,5 +83,12 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('user_types');
+        Schema::dropIfExists('images');
+        Schema::dropIfExists('skills');
+        Schema::dropIfExists('user_skills');
+        Schema::dropIfExists('swipes');
+        Schema::dropIfExists('matchs');
+        Schema::dropIfExists('messages');
     }
 };
