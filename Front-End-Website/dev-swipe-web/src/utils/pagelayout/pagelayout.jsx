006fa8d83c7f29ebef2f15a551a39d667147555e
User <majedhabli2020@gmail.com>
2023-09-10 01:38:32 +0300

    Client-Web <> Updated the page outlet and added styling.

--- Front-End-Website/dev-swipe-web/src/utils/pagelayout/pagelayout.jsx
+++ Front-End-Website/dev-swipe-web/src/utils/pagelayout/pagelayout.jsx
@@ -7,10 +7,16 @@ const PageLayout = () => {
 
     return (
         <div className={styles.Parent_layout}>
-            <div className={styles.side_bar_container}>
+            <div className={styles.navbar_container}>
                 <Navbar/>
             </div>
             <div className={styles.outlet_parent}>
+                <div className={styles.top_img}>
+                    <img src="/topright-icon.png" alt="top right backgroung-image" />
+                </div>
+                <div className={styles.btm_img}>
+                    <img src="/btmleft-icon.png" alt="bottom left backgroung-image" />
+                </div>
                 <Outlet/>
             </div>
         </div>
