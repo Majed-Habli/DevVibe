8463e5d3c86b2cd8eeea4937574cf8dcee08c6fc
User <majedhabli2020@gmail.com>
2023-09-10 17:41:33 +0300

    Client-Web <> Created a card component and set up the skeletal structure.

---
+++ Front-End-Website/dev-swipe-web/src/components/user card/card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from './card.module.css';
+import CustomButton from "../custom button/custombutton";
+
+const Card = () =>{
+
+    return(
+        <div className={styles.card_container}>
+            <div className={styles.container_left}>
+                <div className={styles.profile_image_container}>
+                    <img src="/testuser.png" alt="" />
+                </div>
+                <div className={styles.user_name}>majed</div>
+            </div>
+            <div className={styles.container_rigth}></div>
+            <CustomButton title={"view profile"}/>
+        </div>
+    )
+}
