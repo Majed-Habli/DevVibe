8914f23901d196a4e081f4ae6d5e24f535f075d7
User <majedhabli2020@gmail.com>
2023-09-10 17:02:10 +0300

    Client-Web <> Created teh skeletal structure for the dashboard page.

--- Front-End-Website/dev-swipe-web/src/pages/dashboard/dashboard.jsx
+++ Front-End-Website/dev-swipe-web/src/pages/dashboard/dashboard.jsx
@@ -4,8 +4,18 @@ import styles from './dashboard.module.css';
 const Dashboard = () =>{
 
     return(
-        <div>Dashboard</div>
-        // <div className={styles.statement}>Welcome, </div> {/*{localStorageAction("user_name")}*/}
+        <div className={styles.page_container}>
+            <div className={styles.page_header}>
+                <div className={styles.statement}>Welcome, </div> {/*{localStorageAction("user_name")}*/}
+
+            </div>
+            <div className={styles.component_container}>
+                <div className={styles.component_container_left}>
+                    <div className={styles.component_vertical_layout}></div>
+                </div>
+                <div className={styles.component_container_right}></div>
+            </div>
+        </div>
     )
 }
 
