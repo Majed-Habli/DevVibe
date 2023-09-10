d9c1188e44c79a150fc305935a683aaea3df46f5
User <majedhabli2020@gmail.com>
2023-09-11 01:51:05 +0300

    Client-Web <> Updated the donut component code.

--- Front-End-Website/dev-swipe-web/src/components/charts/donut/donut.jsx
+++ Front-End-Website/dev-swipe-web/src/components/charts/donut/donut.jsx
@@ -12,19 +12,59 @@ const Donut = () =>{
     const data  = {
         labels: [],
         datasets: [{
-            label: 'Poll',
-            data: [3, 6],
+            label: '',
+            data: [30,100],
             backgroundColor: ['#F56954', '#EEEEEE'],
         }]
     }
 
     const options = {
-
+        responsive: true,
+        plugins: {
+        tooltip: {
+            enabled: false // <-- this option disables tooltips
+        }
+        }
     }
+
+    const centerText = {
+        id: 'centerText',
+        beforeDatasetsDraw(chart, args, pluginOptions){
+            const {ctx, data} = chart;
+            ctx.save();
+            ctx.font = 'bolder 12px sans-serif';
+            ctx.fillStyle = 'red';
+            ctx.textAlign = 'center';
+            ctx.textBaseLine = 'middle';
+            ctx.fillText(data.datasets[0].data[0], chart.getDatasetMeta(0).data[0].x, chart.getDatasetMeta(0).data[0].y);
+        }
+    }
+
+    // const backgroundCircle = {
+    //     id: 'backgroundCircle',
+    //     beforeDatasetsDraw(chart, args, pluginOptions){
+    //         const {ctx} = chart;
+    //         ctx.save();
+
+    //         const x_coord = chart.getDatasetMeta(0).data[0].x;
+    //         const y_coord = chart.getDatasetMeta(0).data[0].y;
+    //         const inner_rad = chart.getDatasetMeta(0).data[0].innerRadius;
+    //         const outer_rad = chart.getDatasetMeta(0).data[0].outerRadius;
+    //         const angle = Math.PI / 180;
+    //         const width = outer_rad - inner_rad;
+
+    //         ctx.beginPath();
+    //         ctx.lineWidth = width;
+    //         ctx.strokeStyle = '#EEEEEE';
+    //         ctx.arc(x_coord, y_coord, outer_rad - width / 2, 0, angle * 360, true)
+    //         ctx.stroke();
+
+    //     }
+    // } backgroundCircle
 
     return(
         <div className={styles.container}>
-            <Doughnut data={data} options={options}/>
+            <Doughnut data={data} options={options} plugins={[ centerText]}/>
         </div>
     )
 }
