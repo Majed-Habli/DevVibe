8a1542ce639c1f788096c635fcd4eccf2acc7fec
User <majedhabli2020@gmail.com>
2023-09-11 00:41:29 +0300

    Client-Web <> Updated analytics component styling and updated the styling for the dashboard page.

--- Front-End-Website/dev-swipe-web/src/pages/dashboard/dashboard.jsx
+++ Front-End-Website/dev-swipe-web/src/pages/dashboard/dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from './dashboard.module.css';
 import InterestedTable from "../../components/interested users/interested";
 import MatchedTable from "../../components/matched users/matched";
+import AnalyticsComponent from "../../components/analytics/analytics";
 
 const Dashboard = () =>{
 
@@ -15,6 +16,7 @@ const Dashboard = () =>{
                 <div className={styles.component_container_left}>
                     <div className={styles.component_vertical_layout}>
                         <InterestedTable/>
+                        <AnalyticsComponent/>
                     </div>
                 </div>
                 <div className={styles.component_container_right}>
