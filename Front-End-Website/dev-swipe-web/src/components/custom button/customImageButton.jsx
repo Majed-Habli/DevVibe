3a71ee48ef0aba7c7df6cd91576b054343d473bf
User <majedhabli2020@gmail.com>
2023-09-11 16:07:07 +0300

    Client-Web <> Updated the custom image component and profile page styling.

--- Front-End-Website/dev-swipe-web/src/components/custom button/customImageButton.jsx
+++ Front-End-Website/dev-swipe-web/src/components/custom button/customImageButton.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-const CustomImageButton = ({image_name, width, height, backgroundColor, borderRadius, display, alignItems, justifyContent, onClick ,image_width, image_height}) => {
+const CustomImageButton = ({image_name, width, height, backgroundColor, borderRadius, display, alignItems, justifyContent, onClick ,image_width, image_height, text, columnGap, padding, boxShadow}) => {
 
     const image_url = `/${image_name}`;
+    const txt = text;
 
     return(
         <div style={{width, height, backgroundColor, borderRadius, 
-        cursor: 'pointer', display, alignItems, justifyContent}}
+        cursor: 'pointer', display, alignItems, justifyContent, columnGap, padding, boxShadow}}
         onClick={onClick}>
             <img style={{width: image_width,height: image_height}} src= {image_url} alt="" />
+            {txt &&(
+                <div>{text}</div>
+            )}
         </div>
     )
 }
--- Front-End-Website/dev-swipe-web/src/pages/profile/profile.jsx
+++ Front-End-Website/dev-swipe-web/src/pages/profile/profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from './profile.module.css';
 import HeaderComp from "../../components/profile header/headercard";
+import CustomImageButton from "../../components/custom button/customImageButton";
 
 const Profile = () =>{
 
@@ -10,6 +11,10 @@ const Profile = () =>{
                 <HeaderComp/>
             </div>
             <div className={styles.component_container}>
+                <div className={styles.button_container}>
+                    <CustomImageButton text={'upload new image'} width={190} height={34} display={'flex'} alignItems={'center'} columnGap={'1rem'} image_name={"Vector.png"} image_height={16} image_width={16} backgroundColor={'white'} padding={'0 .5rem'} borderRadius={4} boxShadow={'0 2px 16px 0 rgba(0, 0, 0, 0.1), 0 2px 8px 0 rgba(0, 0, 0, 0.1)'}/>
+                    <CustomImageButton text={'upload new resume'} width={190} height={34} display={'flex'} alignItems={'center'} columnGap={'1rem'} image_name={"Vector.png"} image_height={16} image_width={16} backgroundColor={'white'} padding={'0 .5rem'} borderRadius={4} boxShadow={'0 2px 16px 0 rgba(0, 0, 0, 0.1), 0 2px 8px 0 rgba(0, 0, 0, 0.1)'}/>
+                </div>
             </div>
         </div>
     )
