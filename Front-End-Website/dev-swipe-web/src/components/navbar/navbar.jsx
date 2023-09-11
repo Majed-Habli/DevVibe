b1d3a707b716b718f37a793d95c6b0a65f5d0698
User <majedhabli2020@gmail.com>
2023-09-11 03:04:51 +0300

    Client-Web <> Updated navbar component.

--- Front-End-Website/dev-swipe-web/src/components/navbar/navbar.jsx
+++ Front-End-Website/dev-swipe-web/src/components/navbar/navbar.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from './navbar.module.css';
 import CustomButton from "../custom button/custombutton";
+import { localStorageAction } from "../../utils/functions/localStorage";
 
 const Navbar = () => {
+    let [token, setToken] = useState('');
+    const validate = () =>{
+        setToken=(localStorageAction("token"));
+        console.log('the token was read', token);
+        // localStorage.removeItem('token');
+    }
+
+    useEffect(()=>{
+        validate();
+    },[]);
 
     return(
         <div className={styles.navbar_container}>
@@ -10,7 +21,18 @@ const Navbar = () => {
                 <div className={styles.logo_container}>
                     <img src="/Logo.png" alt="brand logo" />
                 </div>
-                <CustomButton title={'Login'} width={93} height={27} borderRadius={4} textAlign={'center'} backgroundColor={'#FCC860'}/>
+                {token ? (
+                    <CustomButton title={'Login'} width={93} height={27} borderRadius={4} textAlign={'center'} backgroundColor={'#FCC860'}/>
+                ):(
+                    <div className={styles.card_container}>
+                        <div className={styles.container_left}>
+                            <div className={styles.profile_image_container}>
+                                <img src="/testuser.png" alt="profile image" />
+                            </div>
+                            <div className={styles.user_name}>majed</div>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     )
