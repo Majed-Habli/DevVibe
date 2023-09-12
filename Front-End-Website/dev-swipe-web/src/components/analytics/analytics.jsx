55ed92bf2401a3b1efb814c01f1136d3c1a7087d
User <majedhabli2020@gmail.com>
2023-09-12 22:28:12 +0300

    Client-Web <> Added functionality to the analytics  component.

--- Front-End-Website/dev-swipe-web/src/components/analytics/analytics.jsx
+++ Front-End-Website/dev-swipe-web/src/components/analytics/analytics.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from './analytics.module.css';
 import Donut from "../charts/donut/donut";
+import { requestMethods } from "../../utils/functions/requestMethods.";
+import { sendRequest } from "../../utils/functions/axios";
+import { localStorageAction } from "../../utils/functions/localStorage";
 
 const AnalyticsComponent = () =>{
+    const [stats, setStats] = useState([]);
+    const [error,setError] = useState('')
+
+    const getStats = async () =>{
+        const token = localStorageAction("token");
+        const userId = localStorageAction("user_id");
+
+        try {
+            if(!token){
+                setError('there is nothing to show here');
+                console.log(error);
+            }else{
+
+                const response = await sendRequest({
+                    route: "/user/developer/analysis",
+                    method: requestMethods.GET,
+                });
+                const data = response;
+                console.log("analysis res", response)
+                const token = " ";
+    
+                if(data.status == 'success'){
+                    const obj = data;
+                    console.log("analysis obj",obj)
+                    setStats(obj);
+
+                }else{
+                    setError("failed to get user data!");
+                    console.log(error);
+                }
+            }
+            
+          } catch (error) {
+            console.error("failed to get user:", error);
+          }
+    }
+    console.log(stats.matched_count)
+
+    useEffect(()=>{
+        // setStats({liked_count: stats && stats.liked_count ? stats.liked_count: false,matched_count: stats && stats.matched_count ? stats.matched_count: false,skipped_count: stats && stats.skipped_count ? stats.skipped_count: false,viewed_count: stats && stats.viewed_count ? stats.viewed_count: false,})
+        getStats();
+    },[]);
 
     return(
         <div className={styles.container}>
@@ -15,15 +60,15 @@ const AnalyticsComponent = () =>{
                     <div>Viewed</div>
                 </div>
                 <div className={styles.chart_container}>
-                    <Donut value={55} backgroundColor={'#3C8DBC'}/>
+                    {stats.matched_count || !stats.matched_count &&(<Donut value={stats.matched_count} backgroundColor={'#3C8DBC'}/>)}
                     <div>matched</div>
                 </div>
                 <div className={`${styles.chart_container} ${styles.spacing}`}>
-                    <Donut value={13} backgroundColor={'#00A65A'}/>
+                    {stats.skipped_count || !stats.skipped_count  &&(<Donut value={stats.skipped_count} backgroundColor={'#00A65A'}/>)}
                     <div>skipped</div>
                 </div>
                 <div className={styles.chart_container}>
-                    <Donut value={70} backgroundColor={'#39CCCC'}/>
+                    {stats.liked_count &&(<Donut value={stats.liked_count} backgroundColor={'#39CCCC'}/>)}
                     <div>liked</div>
                 </div>
             </div>
