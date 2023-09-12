a1472068e299c4278f952db262b26de9e4dbe308
User <majedhabli2020@gmail.com>
2023-09-12 21:42:46 +0300

    Client-Web <> Added functionality to the card component.

--- Front-End-Website/dev-swipe-web/src/components/interested users/interested.jsx
+++ Front-End-Website/dev-swipe-web/src/components/interested users/interested.jsx
@@ -1,16 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from './interested.module.css'
 import CustomButton from "../custom button/custombutton";
 import Card from "../user card/card";
 import PopUpCard from "../popup card/popupcard";
 import ViewAllPopUp from "../models/view users/viewall";
+import { localStorageAction } from "../../utils/functions/localStorage";
+import { requestMethods } from "../../utils/functions/requestMethods.";
+import { sendRequest } from "../../utils/functions/axios";
 
 const InterestedTable = () => {
     const [showModel, setShowModel] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [error,setError] = useState('')
 
     const veiwAll = async () =>{
         setShowModel(true);
     }
+
+    const getInterestes = async () =>{
+        const token = localStorageAction("token");
+        const userId = localStorageAction("user_id");
+
+        try {
+            if(!token){
+                setError('there is nothing to show here');
+                console.log(error);
+            }else{
+
+                const response = await sendRequest({
+                    route: "/user/developer/view_interested",
+                    method: requestMethods.GET,
+                });
+                const data = response;
+                console.log("hello there", response)
+                const token = " ";
+    
+                if(data.status == 'success'){
+                    const obj = data.data;
+                    console.log("here is the onj",obj)
+                    setUsers(obj);
+
+                }else{
+                    setError("failed to get user data!");
+                    console.log(error);
+                }
+            }
+            
+          } catch (error) {
+            console.error("failed to get user:", error);
+          }
+    }
+
+    useEffect(()=>{
+        getInterestes();
+    },[]);
+
 
     return(
         <div className={styles.table_container}>
@@ -19,7 +63,7 @@ const InterestedTable = () => {
                 <CustomButton title={'view all'} onClick={veiwAll}/>
             </div>
             <div className={styles.table_body}>
-                <Card button={true}/>
+                <Card data={users} button={true}/>
             </div>
             {showModel && (
                 <div className={styles.popup_background}>
--- Front-End-Website/dev-swipe-web/src/components/user card/card.jsx
+++ Front-End-Website/dev-swipe-web/src/components/user card/card.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import styles from './card.module.css';
 import CustomButton from "../custom button/custombutton";
 
-const Card = ({button}) =>{
+const Card = ({button, data}) =>{
 
     return(
-        <div className={styles.card_container}>
-            <div className={styles.container_left}>
-                <div className={styles.profile_image_container}>
-                    <img src="/testuser.png" alt="" />
+        <div>
+            {data.map((dat)=>(
+            <div key={dat.id} className={styles.card_container}>
+                <div className={styles.container_left}>
+                    <div className={styles.profile_image_container}>
+                        <img src="/testuser.png" alt="profile image" />
+                    </div>
+                    <div className={styles.user_name}>{dat.user_name}</div>
                 </div>
-                <div className={styles.user_name}>majed</div>
+                {button &&(
+                    <CustomButton title={"view profile"}/>
+                )}
             </div>
-            {button &&(
-                <CustomButton title={"view profile"}/>
-            )}
+            ))}
         </div>
+        
+
     )
 }
 
