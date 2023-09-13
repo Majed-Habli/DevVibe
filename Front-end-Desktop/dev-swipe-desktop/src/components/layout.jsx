8bc38f72d13a26f0496699b619933cede9bce008
User <majedhabli2020@gmail.com>
2023-09-14 00:21:11 +0300

    Client-Desktop <> Updated Layout component.

--- Front-end-Desktop/dev-swipe-desktop/src/components/layout.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/components/layout.jsx
@@ -5,6 +5,15 @@ const Layout = () => {
     return(
         <div>
             <p>this is our layout</p>
+
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/work">Work</a>
+                </li>
+            </ul>
         </div>
     )
 }
