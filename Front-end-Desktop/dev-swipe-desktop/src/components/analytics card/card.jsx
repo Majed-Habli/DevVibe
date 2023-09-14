3d4bbc96a49c48a5f9f4b13632e776ffde0cd6e6
User <majedhabli2020@gmail.com>
2023-09-14 16:08:50 +0300

    Client-Desktop <> Updated the card component skeletal structure.

--- Front-end-Desktop/dev-swipe-desktop/src/components/analytics card/card.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/components/analytics card/card.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import styles from '../../styles/analytics-card.module.css';
 import { TbUsersPlus } from 'react-icons/tb';
+import { BsFillArrowRightCircleFill } from 'react-icons/bs';
 
 const AnalyticsCard = () => {
 
     return(
         <div className={styles.container}>
-            <div className={styles.left_container}>
-                <div className={styles.number_container}>120</div>
-                <div className={styles.statement}>new users</div>
+            <div className={styles.top_container}>
+                <div className={styles.left_container}>
+                    <div className={styles.number_container}>120</div>
+                    <div className={styles.statement}>new users</div>
+                </div>
+                <div className={styles.right_container}>
+                    <TbUsersPlus fill="white" color="white" size={80}/>
+                </div>
             </div>
-            <div className={styles.right_container}>
-                <TbUsersPlus fill="white" color="white" size={80}/>
+            <div className={styles.cta}>
+                <span>More info</span>
+                <BsFillArrowRightCircleFill fill="white" color="white"/>
             </div>
         </div>
     )
