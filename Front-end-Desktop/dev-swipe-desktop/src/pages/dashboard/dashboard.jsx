9b77c7997fdb25e16f5d5cc64962d3be7bff20f1
User <majedhabli2020@gmail.com>
2023-09-14 17:23:08 +0300

    Client-Desktop <> Added functionality to dashboard.

--- Front-end-Desktop/dev-swipe-desktop/src/pages/dashboard/dashboard.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/pages/dashboard/dashboard.jsx
@@ -2,11 +2,44 @@ import React, { useState } from "react";
 import styles from '../../styles/dashboard.module.css'
 import AnalyticsCard from "../../components/analytics card/card";
 import InfoBox from "../../components/widgets/info-box";
+import { sendRequest } from "../../utils/functions/axios";
+import { requestMethods } from "../../utils/functions/requestMethods.";
+import { localStorageAction } from "../../utils/functions/localStorage";
 
 const Dashboard = () =>{
     const [error, setError] = useState('');
+    const [info, setInfo] = useState('');
 
+    const getAnalytics = async () =>{
+        const token = localStorageAction("token");
+
+        try {
+            if(!token){
+                setError('there is nothing to show here');
+                console.log(error);
+            }else{
+
+                const response = await sendRequest({
+                    route: '/user/admin/analytics',
+                    method: requestMethods.GET,
+                });
+                const data = response;
+                const token = " ";
     
+                if(data.status == 'success'){
+                    const obj = data.data;
+                    setInfo(obj);
+
+                }else{
+                    setError("failed to get data!");
+                    console.log(error);
+                }
+            }
+            
+          } catch (error) {
+            console.error("Api returned with a fail:", error);
+          }
+    }
 
     return(
         <div className={styles.container}>
