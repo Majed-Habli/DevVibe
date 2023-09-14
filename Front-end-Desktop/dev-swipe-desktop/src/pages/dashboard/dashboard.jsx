cf2ba3151b3de467b8c36fedb7e7295ff6d50237
User <majedhabli2020@gmail.com>
2023-09-14 17:08:18 +0300

    Client-Desktop <> Updated Dashboard page.

--- Front-end-Desktop/dev-swipe-desktop/src/pages/dashboard/dashboard.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/pages/dashboard/dashboard.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from '../../styles/dashboard.module.css'
 import AnalyticsCard from "../../components/analytics card/card";
 import InfoBox from "../../components/widgets/info-box";
 
 const Dashboard = () =>{
+    const [error, setError] = useState('');
+
+    
 
     return(
         <div className={styles.container}>
