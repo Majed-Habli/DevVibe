89fd7c2403f7365bb3df40b50116b9632c7119d5
User <majedhabli2020@gmail.com>
2023-09-14 21:23:43 +0300

    Client-Desktop <> Created a new custom input component.

---
+++ Front-end-Desktop/dev-swipe-desktop/src/components/custom input/custominput.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const CustomInput = ({label, type, name, value, width, height, color, handleChange, fontSize, fontWeight, textArea, placeholder}) => {
+
+    return(
+        <div style={{display: 'flex', flexDirection: 'column', rowGap: '5px'}}>
+            <label style={{fontSize, fontWeight}} htmlFor={label}>
+                {label}
+            </label>
+            {!textArea ?(<input
+                id={label}
+                placeholder={placeholder}
+                onChange={handleChange}
+                name={name}
+                style={{width, height, padding: '1rem', backgroundColor: '#ffffff', border: '1px solid #9F8484', borderRadius: 4, color}}
+                type={type}
+                value={value}/>):(
+                    <textarea id={label} name={name} placeholder={placeholder} cols="30" rows="10" style={{width, height, padding: '1rem', backgroundColor: '#ffffff', border: '1px solid #9F8484', borderRadius: 4, color}} 
+                    value={value} onChange={handleChange}/>
+                )}
+        </div>
+    )
+}
+
+export default CustomInput;
