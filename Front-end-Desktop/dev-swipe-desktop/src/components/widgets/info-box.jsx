4031a9b3e346aeac028ccc54d0b40ee9ecc11144
User <majedhabli2020@gmail.com>
2023-09-14 16:46:41 +0300

    Client-Desktop <> Updated infobox component's skeletal structure.

--- Front-end-Desktop/dev-swipe-desktop/src/components/widgets/info-box.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/components/widgets/info-box.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import styles from '../../styles/info-box.module.css';
+import { AiOutlineUser } from 'react-icons/ai';
 
 const InfoBox = () => {
 
     return(
-        <div></div>
+        <div className={styles.container}>
+            <div className={styles.icon}>
+                <AiOutlineUser fill="white" />
+            </div>
+            <div className={styles.right_container}>
+                <span className={styles.span_header}>Info header</span>
+                <span className={styles.span_count}>count</span>
+            </div>
+        </div>
     )
 }
 
