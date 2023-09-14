3f7feec5ee744e50af6cacbdd80d05c7f8ebe1e6
User <majedhabli2020@gmail.com>
2023-09-14 16:42:30 +0300

    Client-Desktop <> Created a new widget component.

---
+++ Front-end-Desktop/dev-swipe-desktop/src/components/widgets/info-box.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from '../../styles/info-box.module.css';
+
+const InfoBox = () => {
+
+    return(
+        <div></div>
+    )
+}
+
+export default InfoBox;
