2929c4c6bd05ad34a6413c14c772e7499c18e6a3
User <majedhabli2020@gmail.com>
2023-09-14 23:43:41 +0300

    Client-Desktop <> Updated info-box component.

--- Front-end-Desktop/dev-swipe-desktop/src/components/widgets/info-box.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/components/widgets/info-box.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import styles from '../../styles/info-box.module.css';
 import { AiOutlineUser } from 'react-icons/ai';
+import { TbUsersPlus } from 'react-icons/tb';
+import { CgSmartphoneChip } from 'react-icons/cg';
 
-const InfoBox = () => {
+const InfoBox = ({data,type,backgroundColor, title}) => {
 
     return(
         <div className={styles.container}>
-            <div className={styles.icon}>
-                <AiOutlineUser fill="white" size={30}/>
+            <div className={styles.icon} style={{backgroundColor}}>
+            {type == 'devs' &&(
+                <AiOutlineUser color="white" size={30}/>
+            )}
+            {type == 'recs' &&(
+                <TbUsersPlus color="white" size={30}/>
+                
+                )}
+            {type == 'skills' &&(
+                <CgSmartphoneChip color="white" size={30}/>
+                )}
+            {type == 'countries' &&(
+                <TbUsersPlus color="white" size={30}/>
+            )}
+
+                
             </div>
             <div className={styles.right_container}>
-                <span className={styles.span_header}>Info header</span>
-                <span className={styles.span_count}>count</span>
+                <span className={styles.span_header}>{title}</span>
+                <span className={styles.span_count}>{data}</span>
             </div>
         </div>
     )
