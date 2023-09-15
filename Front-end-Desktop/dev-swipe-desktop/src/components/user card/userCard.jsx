43d396ee562276433fb8d4dd7814ef424d08d578
User <majedhabli2020@gmail.com>
2023-09-15 16:24:41 +0300

    Client-Desktop <> Created a user card component.

---
+++ Front-end-Desktop/dev-swipe-desktop/src/components/user card/userCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from '../../styles/usercard.module.css';
+import CustomButton from '../custom button/custombutton';
+
+const UserCard = () =>{
+
+    return(
+        <div className={styles.container}>
+            <div>role</div>
+            <div className={styles.user_details}>
+                <div className={styles.left_container}>
+                    <div className={styles.user_name}>Majed Habli</div>
+                    <div className={styles.user_description}><span>About:</span>description</div>
+                    <div className={styles.extra_description}>
+                        <div className={styles.row}>
+                            <img src="" alt="icon-address" />
+                            <div>country</div>
+                        </div>
+                        <div className={styles.row}>
+                            <img src="" alt="icon-email" />
+                            <div>email</div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.right_container}>
+                    <img src="/rb2.png" alt="user-profile" />
+                </div>
+            </div>
+            <div className={styles.cto}>
+                <CustomButton title={'view Profile'}/>
+            </div>
+        </div>
+    )
+}
+
+export default UserCard;
