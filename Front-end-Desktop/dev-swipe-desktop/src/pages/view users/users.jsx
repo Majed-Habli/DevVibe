6f3473ef98d20d72887c0360983d9df2c3af2041
User <majedhabli2020@gmail.com>
2023-09-15 15:55:31 +0300

    Client-Desktop <> Updated skeletal structure for the user page.

--- Front-end-Desktop/dev-swipe-desktop/src/pages/view users/users.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/pages/view users/users.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import styles from '../../styles/users.module.css';
 
 const Users = () => {
+    const [search, setSearch] = useState('');
 
     return(
         <div className={styles.container}>
             <div className={styles.page_header}>
-                <span>Dashboard</span>
+                <span>New Developers</span>
+            </div>
+            <div className={styles.searchable}>
+                <div className={styles.top_bar}>
+                    <input type="text" placeholder="Search users here..." value={search} onChange={inputChange}/>
+                </div>
             </div>
         </div>
     )
