27ae62c18ef856f796da9de2c88477a5226e528e
User <majedhabli2020@gmail.com>
2023-09-15 17:23:00 +0300

    Client-Desktop <> Updated user page functionality.

--- Front-end-Desktop/dev-swipe-desktop/src/pages/view users/users.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/pages/view users/users.jsx
@@ -1,14 +1,51 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import styles from '../../styles/users.module.css';
 import UserCard from "../../components/user card/userCard";
+import { sendRequest } from "../../utils/functions/axios";
+import { requestMethods } from "../../utils/functions/requestMethods.";
+import { localStorageAction } from "../../utils/functions/localStorage";
 
 const Users = () => {
     const [search, setSearch] = useState('');
     const [error, setError] = useState('');
+    const [info,setInfo] = useState([]);
 
     const inputChange = (event) => {
         setSearch(event.target.value);
     };
+
+    const getNewDevelopers = async () =>{
+        const token = localStorageAction("token");
+
+        try {
+            if(!token){
+                setError('there is nothing to show here');
+                console.log(error);
+            }else{
+
+                const response = await sendRequest({
+                    route: '/user/admin/new_developers',
+                    method: requestMethods.GET,
+                });
+                const data = response;
+                const token = " ";
+    
+                if(data.status == 'success'){
+                    setInfo(data.data);
+
+                }else{
+                    setError("failed to get data!");
+                    console.log(error);
+                }
+            }
+            
+          } catch (error) {
+            console.error("Api returned with a fail:", error);
+          }
+    }
+    useEffect(()=>{
+        getNewDevelopers();
+    },[]);
 
     return(
         <div className={styles.container}>
@@ -20,7 +57,9 @@ const Users = () => {
                     <input type="text" placeholder="Search users here..." value={search} onChange={inputChange}/>
                 </div>
                 <div className={styles.users_container}>
-                    <UserCard/>
+                    {info.map((inf)=>(
+                        <UserCard key={inf.id} data={inf}/>
+                    ))}
                 </div>
             </div>
         </div>
