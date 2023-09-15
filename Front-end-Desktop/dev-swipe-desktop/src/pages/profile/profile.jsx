c3c71ed2ca6d712213928478e82b81532a5d6834
User <majedhabli2020@gmail.com>
2023-09-16 01:49:59 +0300

    Client-Desktop <> Cleaned the profile component.

--- Front-end-Desktop/dev-swipe-desktop/src/pages/profile/profile.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/pages/profile/profile.jsx
@@ -7,15 +7,11 @@ import { sendRequest } from "../../utils/functions/axios";
 import { requestMethods } from "../../utils/functions/requestMethods.";
 import { localStorageAction } from "../../utils/functions/localStorage";
 import { useParams } from "react-router-dom";
-// import ViewImages from "../../components/models/images form/viewimages";
-// import ViewUpload from "../../components/models/view upload/viewUpload";
 
 const Profile = () =>{
     const [errorSkills, setErrorSkills] = useState('');
     const [errorImages, setErrorImages] = useState('');
     
-    const [showModel, setShowModel] = useState(false);
-    const [showUploadModel, setShowUploadModel] = useState(false);
     const [skills, setSkills] = useState([]);
     const [images, setImages] = useState([]);
     const [error, setError] = useState('');
@@ -122,13 +118,6 @@ const Profile = () =>{
             console.error("failed to get user:", error);
           }
     }
-    
-    const viewModel = async() =>{
-        setShowModel(true)
-    }
-    const viewModelUpload = async() =>{
-        setShowUploadModel(true)
-    }
 
     useEffect(()=>{
         getUser();
@@ -144,12 +133,7 @@ const Profile = () =>{
                     <HeaderComp data={user}/>
                 </div>
                 <div className={styles.button_container}>
-                    <CustomImageButton text={'view images'} width={'fit-content'} height={34} display={'flex'} alignItems={'center'} columnGap={'1rem'} image_name={"gallery.png"} image_height={16} image_width={16} backgroundColor={'white'} padding={'0 .5rem'} borderRadius={4} boxShadow={'0 2px 16px 0 rgba(0, 0, 0, 0.1), 0 2px 8px 0 rgba(0, 0, 0, 0.1)'} onClick={()=>viewModel()}/>
-
-                        <CustomImageButton text={'upload new image'} width={190} height={34} display={'flex'} alignItems={'center'} columnGap={'1rem'} image_name={"Vector.png"} image_height={16} image_width={16} backgroundColor={'white'} padding={'0 .5rem'} borderRadius={4} boxShadow={'0 2px 16px 0 rgba(0, 0, 0, 0.1), 0 2px 8px 0 rgba(0, 0, 0, 0.1)'} onClick={()=>viewModelUpload()}/>
-
-                    
-                        <CustomImageButton text={'upload new resume'} width={190} height={34} display={'flex'} alignItems={'center'} columnGap={'1rem'} image_name={"Vector.png"} image_height={16} image_width={16} backgroundColor={'white'} padding={'0 .5rem'} borderRadius={4} boxShadow={'0 2px 16px 0 rgba(0, 0, 0, 0.1), 0 2px 8px 0 rgba(0, 0, 0, 0.1)'}/>
+                    <CustomImageButton text={'upload new resume'} width={190} height={34} display={'flex'} alignItems={'center'} columnGap={'1rem'} image_name={"Vector.png"} image_height={16} image_width={16} backgroundColor={'white'} padding={'0 .5rem'} borderRadius={4} boxShadow={'0 2px 16px 0 rgba(0, 0, 0, 0.1), 0 2px 8px 0 rgba(0, 0, 0, 0.1)'}/>
                 </div>
             </div>
             <div className={styles.component_container}>
@@ -168,18 +152,6 @@ const Profile = () =>{
                     </div>
                 </div>
             </div>
-
-            {/* {showModel && (
-                <div className={styles.popup_background}>
-                    <ViewImages isOpen={setShowModel} imgs={images}/>
-                </div>
-            )} */}
-
-            {/* {showUploadModel && (
-                <div className={styles.popup_background}>hey
-                    <ViewUpload isOpen={setShowUploadModel} />
-                </div>
-            )} */}
         </div>
     )
 }
