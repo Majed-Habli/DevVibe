8df96e9d9012449e0076982413aa21cf124b1f7b
User <majedhabli2020@gmail.com>
2023-09-17 21:49:45 +0300

    Client-Desktop <> Updated skills page.

--- Front-end-Desktop/dev-swipe-desktop/src/pages/skills/skills.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/pages/skills/skills.jsx
@@ -59,6 +59,7 @@ const Skills = () => {
             <div className={styles.searchable}>
                 <div className={styles.top_bar}>
                     <input type="text" placeholder="Search users here..." value={search} onChange={inputChange}/>
+                    <CustomButton title={'add skill'} width={70} height={27} display={'flex'} alignItems={'center'} justifyContent={'center'} fontSize={12} fontWeight={600} backgroundColor={'#FCC860'}/>
                 </div>
                 <div className={styles.users_container}>
                     {skills.map((skill)=>(
