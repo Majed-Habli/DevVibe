8c485981b372ebe1defd9d2e148209e6d16993f1
User <majedhabli2020@gmail.com>
2023-09-20 17:21:17 +0300

    Client-Desktop <> Updated the custom image button component.

--- Front-end-Desktop/dev-swipe-desktop/src/components/custom button/customImageButton.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/components/custom button/customImageButton.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const CustomImageButton = ({image_name, width, height, backgroundColor, borderRadius, display, alignItems, justifyContent, onClick ,image_width, image_height, text, columnGap, padding, boxShadow, flexDirection}) => {
+const CustomImageButton = ({image_name, width, height, backgroundColor, borderRadius, display, alignItems, justifyContent, onClick ,image_width, image_height, text, columnGap, padding, boxShadow, flexDirection, cursor}) => {
 
     const image_url = `/${image_name}`;
     const txt = text;
 
     return(
         <div style={{width, height, backgroundColor, borderRadius, 
-        cursor: 'pointer', display, alignItems, justifyContent, columnGap, padding, boxShadow, flexDirection}}
+        cursor, display, alignItems, justifyContent, columnGap, padding, boxShadow, flexDirection}}
         onClick={onClick}>
             <img style={{width: image_width,height: image_height}} src= {image_url} alt="" />
             {txt &&(
