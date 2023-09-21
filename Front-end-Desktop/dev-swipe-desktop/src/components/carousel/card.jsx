37a2f3e9157b70e33f1d54bf29611c2271279db6
User <majedhabli2020@gmail.com>
2023-09-21 03:28:37 +0300

    Client-Web <> Added a new carousel component.

---
+++ Front-end-Desktop/dev-swipe-desktop/src/components/carousel/card.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import '../../styles/cardCarousel.css';
+
+import { Navigation, Pagination, EffectCoverflow, Mousewheel } from 'swiper/modules';
+
+const CarouselComp = ({value ,issue}) => {
+  const [error,setError] = useState('');
+  const openImage = () =>{
+      console.log('hi')
+  }
+
+  useEffect(()=>{
+    if(issue){
+      setError(issue);
+    }
+  },[issue])
+
+  return (
+    <>
+      {!error ? (<Swiper
+        direction='vertical'
+        slidesPerView={1}
+        spaceBetween={10}
+        mousewheel={true}
+        pagination={{
+          clickable: false,
+        }}
+
+        modules={[Mousewheel,Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {value.map((val)=>(
+            <SwiperSlide key={val.id} onClick={openImage}>
+                <img src={`${val.image_url}`} alt="user images" />
+            </SwiperSlide>
+        ))}
+        
+      </Swiper>):(
+        <div className='error_message'>{error}</div>
+      )}
+    </>
+  );
+}
+
+export default CarouselComp;
