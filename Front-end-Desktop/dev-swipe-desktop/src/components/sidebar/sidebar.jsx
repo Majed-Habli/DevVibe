89dfab03c649b4345d8d6eb265d94267959906a5
User <majedhabli2020@gmail.com>
2023-09-21 05:18:06 +0300

    Client-Web <> Updated sidebar component.

--- Front-end-Desktop/dev-swipe-desktop/src/components/sidebar/sidebar.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/components/sidebar/sidebar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styles from '../../styles/sidebar.module.css';
 import { AiOutlineUser } from 'react-icons/ai';
 import { AiOutlineDashboard } from 'react-icons/ai';
+import CustomButton from "../custom button/custombutton";
 
 const Sidebar = ({status}) => {
     const [showSubMenu, setShowSubMenu] = useState([]);
@@ -22,129 +23,135 @@ const Sidebar = ({status}) => {
 
     return(
         <div className={`${navbarOpen? styles.show_menu: styles.container}`}>
-            <div className={styles.logo}>
-                <img src="/Logo2-0.png" alt="app logo" />
-            </div>
+            <div>
 
-            <ul className={styles.sidebar}>
-                <li className={styles.header}>Main navigation</li>
-                
-                <li className={styles.treeview}>
-                    <div className={styles.category_container} onClick={()=>{toggleShowMenu("Dashboard")}}>
-                        <div>
-                            {isCategoryExpanded("Dashboard") ?(
-                                <label className={styles.treatitle} htmlFor="dashboardToggle">
-                                    <AiOutlineDashboard fill="white" />
-                                    <span className={styles.selected}>Dashboard</span>
-                                </label>
-                                ):(
-                                    <label className={styles.treatitle} htmlFor="dashboardToggle">
-                                        <AiOutlineDashboard/>
-                                        <span>Dashboard</span>
-                                    </label>
-                                )
-                            }
-                        </div>
-                        <img className={`${styles.arrow} ${isCategoryExpanded("Dashboard") ? 'expanded' : ''}`} src="/arrow.png" alt="arrow icon"/>
-                    </div>
-                    {isCategoryExpanded("Dashboard") && (
-                    <div className={styles.cont}>
+                <div className={styles.logo}>
+                    <img src="/Logo2-0.png" alt="app logo" />
+                </div>
 
-                        <ul className={styles.treeview_menu}>
-                            <li>
-                                <a href="/dashboard">
-                                    <img src="" alt="" />
-                                    <span className={styles.list_headers}>Summary</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/dashboard/stats">
-                                    <img src="" alt="" />
-                                    <span className={styles.list_headers}>Stats</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    )}
-                </li>
-                <li className={styles.treeview}>
-                    <div className={styles.category_container}  onClick={()=>{toggleShowMenu("users")}}>
-                        <div>
-                        {isCategoryExpanded("users") ?(
-                                <label className={styles.treatitle} htmlFor="usersToggle">
-                                    <AiOutlineUser fill="white" />
-                                    <span className={styles.selected}>User</span>
-                                </label>
-                                ):(
-                                    <label className={styles.treatitle} htmlFor="usersToggle" >
-                                        <AiOutlineUser/>
-                                        <span>User</span>
-                                    </label >
-                                )
-                            }
-                        </div>
-                        <img className={`${styles.arrow} ${isCategoryExpanded("users") ? 'expanded' : ''}`} src="/arrow.png" alt="arrow icon"/>
-                    </div>
-                    {isCategoryExpanded("users") && (
-                    <div className={styles.cont}>
-
-                        <ul className={styles.treeview_menu}>
-                            <li>
-                                <a href="/dashboard/users/new-developers">
-                                    <img src="" alt="" />
-                                    <span className={styles.list_headers}>New Developers</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/dashboard/users/new-recruiters">
-                                    <img src="" alt="" />
-                                    <span className={styles.list_headers}>New Recruiters</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/dashboard/users/old-developers">
-                                    <img src="" alt="" />
-                                    <span className={styles.list_headers}>Developers</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/dashboard/users/old-recruiters">
-                                    <img src="" alt="" />
-                                    <span className={styles.list_headers}>Recruiters</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    )}
-                </li>
-                <li className={styles.treeview}>
-                    <div className={styles.category_container}  onClick={()=>{toggleShowMenu("Skills")}}>
-                        <div>
-                        {isCategoryExpanded("Skills") ?(
-                                <label className={styles.treatitle} htmlFor="usersToggle">
-                                    <AiOutlineUser fill="white" />
-                                    <a href="/dashboard/skills">
-                                    <img src="" alt="" />
-                                        <span className={styles.selected}>Skills</span>
-                                    </a>
-                                </label>
-                                ):(
-                                    <label className={styles.treatitle} htmlFor="usersToggle" >
-                                        <AiOutlineUser/>
-                                        <a href="/dashboard/skills">
-                                            <img src="" alt="" />
-                                            <span>Skills</span>
-                                        </a>
-                                    </label >
-                                )
-                            }
-                        </div>
-                        {/* <img className={`${styles.arrow} ${isCategoryExpanded("Skills") ? 'expanded' : ''}`} src="/arrow.png" alt="arrow icon"/> */}
-                    </div>
+                <ul className={styles.sidebar}>
+                    <li className={styles.header}>Main navigation</li>
                     
-                </li>
-                
-            </ul>
+                    <li className={styles.treeview}>
+                        <div className={styles.category_container} onClick={()=>{toggleShowMenu("Dashboard")}}>
+                            <div>
+                                {isCategoryExpanded("Dashboard") ?(
+                                    <label className={styles.treatitle} htmlFor="dashboardToggle">
+                                        <AiOutlineDashboard fill="white" />
+                                        <span className={styles.selected}>Dashboard</span>
+                                    </label>
+                                    ):(
+                                        <label className={styles.treatitle} htmlFor="dashboardToggle">
+                                            <AiOutlineDashboard/>
+                                            <span>Dashboard</span>
+                                        </label>
+                                    )
+                                }
+                            </div>
+                            <img className={`${styles.arrow} ${isCategoryExpanded("Dashboard") ? 'expanded' : ''}`} src="/arrow.png" alt="arrow icon"/>
+                        </div>
+                        {isCategoryExpanded("Dashboard") && (
+                        <div className={styles.cont}>
+
+                            <ul className={styles.treeview_menu}>
+                                <li>
+                                    <a href="/dashboard">
+                                        <img src="" alt="" />
+                                        <span className={styles.list_headers}>Summary</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/dashboard/stats">
+                                        <img src="" alt="" />
+                                        <span className={styles.list_headers}>Stats</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        )}
+                    </li>
+                    <li className={styles.treeview}>
+                        <div className={styles.category_container}  onClick={()=>{toggleShowMenu("users")}}>
+                            <div>
+                            {isCategoryExpanded("users") ?(
+                                    <label className={styles.treatitle} htmlFor="usersToggle">
+                                        <AiOutlineUser fill="white" />
+                                        <span className={styles.selected}>User</span>
+                                    </label>
+                                    ):(
+                                        <label className={styles.treatitle} htmlFor="usersToggle" >
+                                            <AiOutlineUser/>
+                                            <span>User</span>
+                                        </label >
+                                    )
+                                }
+                            </div>
+                            <img className={`${styles.arrow} ${isCategoryExpanded("users") ? 'expanded' : ''}`} src="/arrow.png" alt="arrow icon"/>
+                        </div>
+                        {isCategoryExpanded("users") && (
+                        <div className={styles.cont}>
+
+                            <ul className={styles.treeview_menu}>
+                                <li>
+                                    <a href="/dashboard/users/new-developers">
+                                        <img src="" alt="" />
+                                        <span className={styles.list_headers}>New Developers</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/dashboard/users/new-recruiters">
+                                        <img src="" alt="" />
+                                        <span className={styles.list_headers}>New Recruiters</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/dashboard/users/old-developers">
+                                        <img src="" alt="" />
+                                        <span className={styles.list_headers}>Developers</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/dashboard/users/old-recruiters">
+                                        <img src="" alt="" />
+                                        <span className={styles.list_headers}>Recruiters</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        )}
+                    </li>
+                    <li className={styles.treeview}>
+                        <div className={styles.category_container}  onClick={()=>{toggleShowMenu("Skills")}}>
+                            <div>
+                            {isCategoryExpanded("Skills") ?(
+                                    <label className={styles.treatitle} htmlFor="usersToggle">
+                                        <AiOutlineUser fill="white" />
+                                        <a href="/dashboard/skills">
+                                        <img src="" alt="" />
+                                            <span className={styles.selected}>Skills</span>
+                                        </a>
+                                    </label>
+                                    ):(
+                                        <label className={styles.treatitle} htmlFor="usersToggle" >
+                                            <AiOutlineUser/>
+                                            <a href="/dashboard/skills">
+                                                <img src="" alt="" />
+                                                <span>Skills</span>
+                                            </a>
+                                        </label >
+                                    )
+                                }
+                            </div>
+                            {/* <img className={`${styles.arrow} ${isCategoryExpanded("Skills") ? 'expanded' : ''}`} src="/arrow.png" alt="arrow icon"/> */}
+                        </div>
+                        
+                    </li>
+                    
+                </ul>
+            </div>
+            <div className={styles.button_container}>
+                <CustomButton title={'Logout'} width={100} height={35} display={'flex'} alignItems={'center'} justifyContent={'center'} backgroundColor={'#FCC860'}/>
+            </div>
         </div>
     )
 }
