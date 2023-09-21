47d783fb104bbe29e67c1231ca603b7cc5ad7f4a
User <majedhabli2020@gmail.com>
2023-09-21 05:08:32 +0300

    Client-Web <> Updated carousel component page.

--- Front-end-Desktop/dev-swipe-desktop/src/components/carousel/card.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/components/carousel/card.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { Navigate, useNavigate } from 'react-router-dom';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
@@ -11,6 +11,13 @@ import CustomButton from '../custom button/custombutton';
 
 const CardCarouselComp = ({information ,issue}) => {
   const [error,setError] = useState('');
+  const nav = useNavigate();
+
+  console.log(information)
+  const goToPage = (id) =>{
+    console.log(id)
+    nav(`/dashboard/users/profile/${id}`)
+  }
 
   useEffect(()=>{
     if(issue){
@@ -30,24 +37,24 @@ const CardCarouselComp = ({information ,issue}) => {
         }}
 
         modules={[Mousewheel,Pagination, Navigation]}
-        className="mySwiper"
+        className="mySwiper2"
       >
         {information.map((val)=>(
-            <SwiperSlide key={val.id}>
+            <SwiperSlide key={val.id} className='swiper-slide2'>
                 <img src={`${val.user.profile_image_url}`} alt="user images" />
                 <div className='text_container'>
                   
                   <div>{val.user.user_name}</div>
                   {val.user.user_type_id === 2 ? (<div className='user_title'>Developer</div>):(<div className='user_title'>Recruiter</div>)}
                 </div>
-                <div className='button_container'>
-                  <CustomButton title={'Profile'} width={120} height={32} display={'flex'} alignItems={'center'} justifyContent={'center'} fontSize={14} fontWeight={'bold'} backgroundColor={'#FCC860'}/> 
+                <div className='button_container2'>
+                  <CustomButton title={'Profile'} width={120} height={32} display={'flex'} alignItems={'center'} justifyContent={'center'} fontSize={14} fontWeight={'bold'} backgroundColor={'#FCC860'} onClick={()=>goToPage(val.user.id)}/> 
                 </div>
             </SwiperSlide>
         ))}
         
       </Swiper>):(
-        <div className='error_message'>{error}</div>
+        <div className='error_message2'>{error}</div>
       )}
     </>
   );
