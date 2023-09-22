466bec8e77d3c87c422355918ab71d4dd1ca8f44
User <majedhabli2020@gmail.com>
2023-09-22 14:52:29 +0300

    Client-Mobile <> Updated the profile page.

--- Front-End-Mobile/dev-swipe-mobile/pages/profile/profile.jsx
+++ Front-End-Mobile/dev-swipe-mobile/pages/profile/profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text, View, Image, Dimensions} from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View, Image, Dimensions, ScrollView} from 'react-native';
 import CustomInput from '../../components/custom input/customInput';
 import CustomButton from '../../components/custom button/customButton';
 import SwiperComponent from '../../components/swiper/swiper';
@@ -12,8 +12,31 @@ const Profile = () => {
 
     return(
         <SafeAreaView style={styles.container}>
-            {/* <Text>Profile</Text> */}
-            <SwiperComponent/>
+            <ScrollView style={styles.Scroll_view}>
+                <View style={styles.images_container}>
+                    <SwiperComponent/>
+                </View>
+                <View style={styles.profile_body}>
+                    <View style={styles.body_header}>
+                        <Text style={styles.user_name}>Majed habli</Text>
+                        <Text style={styles.user_location}>Saida,Lebanon</Text>
+                        <View style={styles.image_group}>
+                            <View style={styles.image_button}>
+                                <Image style={styles.icons}></Image>
+                            </View>
+                            <View style={styles.image_button}>
+                                <Image style={styles.icons}></Image>
+                            </View>
+                            <View style={styles.image_button}>
+                                <Image style={styles.icons}></Image>
+                            </View>
+                            <View style={styles.image_button}>
+                                <Image style={styles.icons}></Image>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 } 
@@ -22,10 +45,45 @@ export default Profile;
 
 const styles = StyleSheet.create({
     container: {
-    width: windowWidth,
-    height: windowHeight,
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: 'white',
     },
+    Scroll_view: {
+        flex: 1
+    },
+    images_container: {
+        height: 250
+    },
+    profile_body: {
+        height: '100%',
+        backgroundColor: 'pink'
+    },
+    body_header: {
+        height: 150,
+        alignItems: 'center',
+        justifyContent: 'center',
+        // rowGap: 30,
+        backgroundColor: 'lightyellow'
+    },
+    user_name: {
+        fontSize: 28,
+    },
+    image_group: {
+        width: '100%',
+        height: 65,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'lightblue',
+        flexDirection: 'row',
+        columnGap: 10
+    },
+    image_button: {
+        width: 50,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 14,
+        backgroundColor: 'grey'
+    }
     });
