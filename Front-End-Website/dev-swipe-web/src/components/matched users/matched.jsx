19606fbafac884505b599203db733f35928d8d68
User <majedhabli2020@gmail.com>
2023-09-30 19:45:36 +0300

    Client-Web <> Cleaned the matched component.

--- Front-End-Website/dev-swipe-web/src/components/matched users/matched.jsx
+++ Front-End-Website/dev-swipe-web/src/components/matched users/matched.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styles from './matched.module.css';
 import CardCarouselComp from "../carousel/card/card";
-import CustomButton from "../custom button/custombutton";
-import Card from "../user card/card";
-import CustomImageButton from "../custom button/customImageButton";
 import { localStorageAction } from "../../utils/functions/localStorage";
 import { requestMethods } from "../../utils/functions/requestMethods.";
 import { sendRequest } from "../../utils/functions/axios";
@@ -13,7 +10,6 @@ import SyncLoader from "react-spinners/ClipLoader";
 const MatchedTable = () =>{
     const [users, setUsers] = useState([]);
     const [error,setError] = useState('');
-    const [skills, setSkills] = useState([]);
 
     const [loading, setLoading] = useState(false);
     useEffect(()=>{
@@ -56,10 +52,6 @@ const MatchedTable = () =>{
           }
     }
 
-    const viewProfile = ({id}) => {
-        window.location.href = `/dashboard/profile/${id}`
-    }
-
     useEffect(()=>{
         getMatched();
     },[]);
@@ -70,45 +62,6 @@ const MatchedTable = () =>{
             <div className={styles.table_header}>Matched with</div>
             <div className={styles.table_body}>
                 {!loading ? (<div className={styles.inner_table_container}>
-                    {/* <div className={styles.inner_table_header}>
-                        <div className={styles.space}></div>
-                        <div className={`${styles.cell} ${styles.width_username}`}>Developer</div>
-                        <div className={`${styles.cell} ${styles.width_skill}`}>Skills</div>
-                        <div className={`${styles.cell} ${styles.width_profile}`}>Profile</div>
-                    </div>
-                    {!error? (<div className={styles.inner_table_body}>
-                        {users.map((user,index)=>(
-                            <div key={user.id} className={styles.inner_table_row}>
-                                <div className={styles.index}>{index+1}</div>
-                                <div className={styles.user_card}>
-                                    <div className={styles.card_container}>
-                                        <div className={styles.container_left}>
-                                            <div className={styles.profile_image_container}>
-                                                {user.matched_with.profile_image_url ? (
-                                                    <img src={`${user?.matched_with?.profile_image_url}`} alt="profile image" /> 
-                                                ):(
-                                                    <img src="/default-user.png" alt="profile image" />
-                                                )}
-                                            </div>
-                                            <div className={styles.user_name}>{user.matched_with.user_name}</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className={styles.skills_container}>
-                                    <div className={styles.content}>
-                                        {user.skills.map((use)=>(
-                                            <div key={use.id} className={styles.pill}>{use.skill.name}</div>
-                                        ))}
-                                    </div>
-                                </div>
-                                <div className={styles.profile_button}>
-                                    <CustomImageButton key={user.id} image_name={'Profile.png'} width={37} height={37} image_width={27} image_height={27} display={"flex"} alignItems={"center"} justifyContent={"center"} backgroundColor={"#EEEEEE"} onClick={()=>viewProfile({id :user.matched_with.id})} cursor={'pointer'}/>
-                                </div>
-                            </div>
-                        ))}
-                    </div>):(
-                        <div className={styles.error_message}>{error}</div>
-                    )} */}
                     {!error?
                         <CardCarouselComp information={users} issue={error}/>
                         :(<div className={styles.error_message}>{error}</div>)
