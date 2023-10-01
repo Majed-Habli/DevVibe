23e150baea1131f5bcb41f6a7f3d984b6ee32492
User <majedhabli2020@gmail.com>
2023-10-01 21:48:24 +0300

    Client-Desktop <> Cleaned editform component.

--- Front-end-Desktop/dev-swipe-desktop/src/components/edit form/editform.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/components/edit form/editform.jsx
@@ -1,12 +1,7 @@
 import React, {useState,useEffect} from "react";
 import styles from '../../styles/editform.module.css';
 import CustomImageButton from "../custom button/customImageButton";
-// import PopUpCard from "../../popup card/popupcard";
 import CustomInput from "../custom input/custominput";
-// import CustomButton from "../../custom button/custombutton";
-// import { localStorageAction } from "../../../utils/functions/localStorage";
-// import { sendRequest } from "../../../utils/functions/axios";
-// import { requestMethods } from "../../../utils/functions/requestMethods.";
 
 const EditForm = ({isOpen, data}) =>{
     console.log(data)
@@ -45,16 +40,7 @@ const EditForm = ({isOpen, data}) =>{
                     <CustomInput label={"Email"} name={'email'} placeholder={data.email } width={225} height={35} disabled={'disabled'}/>
                     <CustomInput label={"Country"} name={'country'} placeholder={data.country} width={225} height={35} disabled={'disabled'}/>
                 </div>
-                {/* {data.user_type_id == 3 ?( */}
-                    {/* <div className={styles.grouping}> */}
-                        {/* <CustomInput label={"Company Name"} name={'company_name'} placeholder={data.rec_details.company_name} width={275} height={35} disabled={'disabled'}/> */}
-                        {/* <CustomInput label={"Description"} name={'description'} placeholder={data.rec_details.description} width={'100%'} textArea={true} height={135} disabled={'disabled'}/> */}
-                    {/* </div> */}
-                {/* ):( */}
-                    {/* <div className={styles.grouping}> */}
-                        <CustomInput label={"Description"} name={'description'} placeholder={data.description} width={'100%'} textArea={true} height={340} disabled={'disabled'}/>
-                    {/* </div> */}
-                {/* )} */}
+                <CustomInput label={"Description"} name={'description'} placeholder={data.description} width={'100%'} textArea={true} height={340} disabled={'disabled'}/>
             </div>
         </div>
     )
