e9ea666614ffa220f6cb02f518e1c6359c98eb95
User <majedhabli2020@gmail.com>
2023-10-01 22:56:24 +0300

    Server <> Update FCM push notifications.

--- Back-End-Laravel/Server-DevVibe/app/services/FCMService.php
+++ Back-End-Laravel/Server-DevVibe/app/services/FCMService.php
@@ -10,7 +10,10 @@ class FCMService
     {
         $fcm_token ='AAAAlSWK_0o:APA91bFWnkEm-rAoes7WDZg_PiW04v-G8mUuuRqfCXuFeUXZGyTDQRmvDdEM_Xqw-2k7TI6XRaPZqA9Web95mZwKEONRY_gMr4uNvh2vYJK3Hk_0V3jx4HEO-uQLmmJUhNjOLS15naQH';
 
-        Http::acceptJson()->withToken(config($fcm_token))->post(
+        Http::withoutVerifying()->withHeaders([
+            'Authorization' => 'key=' . $fcm_token,
+            'Content-Type' => 'application/json',
+        ])->post(
             'https://fcm.googleapis.com/fcm/send',
             [
                 'to' => $token,
