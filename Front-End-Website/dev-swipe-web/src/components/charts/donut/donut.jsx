5a1ae7cefbc1cfbfcac369c55f92eb020fa447b1
User <majedhabli2020@gmail.com>
2023-10-01 21:28:24 +0300

    Client-Web <> Cleaned donut chart component.

--- Front-End-Website/dev-swipe-web/src/components/charts/donut/donut.jsx
+++ Front-End-Website/dev-swipe-web/src/components/charts/donut/donut.jsx
@@ -22,7 +22,7 @@ const Donut = ({backgroundColor, value}) =>{
         responsive: true,
         plugins: {
         tooltip: {
-            enabled: false // <-- this option disables tooltips F56954
+            enabled: false
         }
         }
     }
@@ -39,28 +39,6 @@ const Donut = ({backgroundColor, value}) =>{
             ctx.fillText(data.datasets[0].data[0], chart.getDatasetMeta(0).data[0].x, chart.getDatasetMeta(0).data[0].y);
         }
     }
-
-    // const backgroundCircle = {
-    //     id: 'backgroundCircle',
-    //     beforeDatasetsDraw(chart, args, pluginOptions){
-    //         const {ctx} = chart;
-    //         ctx.save();
-
-    //         const x_coord = chart.getDatasetMeta(0).data[0].x;
-    //         const y_coord = chart.getDatasetMeta(0).data[0].y;
-    //         const inner_rad = chart.getDatasetMeta(0).data[0].innerRadius;
-    //         const outer_rad = chart.getDatasetMeta(0).data[0].outerRadius;
-    //         const angle = Math.PI / 180;
-    //         const width = outer_rad - inner_rad;
-
-    //         ctx.beginPath();
-    //         ctx.lineWidth = width;
-    //         ctx.strokeStyle = '#EEEEEE';
-    //         ctx.arc(x_coord, y_coord, outer_rad - width / 2, 0, angle * 360, true)
-    //         ctx.stroke();
-
-    //     }
-    // } backgroundCircle
 
     return(
         <div className={styles.container}>
