027f891b32bdb46e0c716af914fd7e2bcfa5bf78
User <majedhabli2020@gmail.com>
2023-10-01 18:56:18 +0300

    Client-Web <> Updated edit form styling and text input.

--- Front-End-Website/dev-swipe-web/src/components/models/edit form/editform.jsx
+++ Front-End-Website/dev-swipe-web/src/components/models/edit form/editform.jsx
@@ -1,247 +1,237 @@
-import React, {useState,useEffect} from "react";
-import styles from './editform.module.css';
+import React, { useState, useEffect } from "react";
+import styles from "./editform.module.css";
 import CustomImageButton from "../../custom button/customImageButton";
 import CustomInput from "../../custom input/custominput";
 import CustomButton from "../../custom button/custombutton";
 import { localStorageAction } from "../../../utils/functions/localStorage";
 import { sendRequest } from "../../../utils/functions/axios";
 import { requestMethods } from "../../../utils/functions/requestMethods.";
-import Select from 'react-select';
+import Select from "react-select";
 
-const EditForm = ({isOpen, data}) =>{
-    const userType = localStorageAction('user_type');
+const EditForm = ({ isOpen, data }) => {
+  const userType = localStorageAction("user_type");
+  const userId = localStorageAction("user_id");
 
-    const [selectedGender, setSelectedGender] = useState(null);
-    
-    const [search, setSearch] = useState('');
-    const [error, setError] = useState('');
-    
-    const [userSkills,setUserSkills] = useState([]);
-    const [selected, setSelected] = useState([]);
-    
-    const [skills, setSkills] = useState([]);
-    console.log("selected are",selected)
-    
-    const [inputs, setInputs] = useState({});
-    
-    const onChangeGenderHandel = (selectedOption) =>{
-        setSelectedGender(selectedOption);
+  const [selectedGender, setSelectedGender] = useState(null);
+
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState("");
+
+  const [userSkills, setUserSkills] = useState([]);
+  const [selected, setSelected] = useState([]);
+  const [unselectedSkills, setUnselectedSkills] = useState([]);
+
+
+  const [skills, setSkills] = useState([]);
+
+  const [inputs, setInputs] = useState({});
+
+  const onChangeGenderHandel = (selectedOption) => {
+    setSelectedGender(selectedOption);
+  };
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const options = [
+    { value: "male", label: "Male" },
+    { value: "female", label: "Female" },
+  ];
+
+  useEffect(() => {
+    getUserSkills();
+  }, []);
+
+  useEffect(() => {
+    getSkills();
+  }, [search, userType]);
+
+  useEffect(() => {
+    const selectedSkills = skills.filter((skill) =>
+      selected.includes(skill.id)
+    );
+    setUserSkills(selectedSkills);
+  }, [selected, skills]);
+
+  const inputChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const onChangeHandler = (id) => () => {
+    if (selected.includes(id)) {
+      setSelected(selected.filter((x) => x !== id));
+      setUnselectedSkills((prevIds) => [...prevIds, id]);
+    } else {
+      setSelected([...selected, id]);
     }
-    
-    const handleChange = (e) => {
-        setInputs((prev) => ({
-            ...prev,
-            [e.target.name]: e.target.value
-        }));
-    };
+  };
 
-    const options = [
-    { value: 'male', label: 'Male' },
-    { value: 'female', label: 'Female' },
-    ];
+  const hideModel = () => {
+    isOpen((prev) => !prev);
+  };
 
+  const getSkills = async () => {
+    const token = localStorageAction("token");
 
-    console.log("inputs",inputs)
+    try {
+      if (!token) {
+        setError("there is nothing to show here");
+        console.log(error);
+      } else {
+        const response = await sendRequest({
+          route: `/user/developer/view_all_skills/${search}`,
+          method: requestMethods.GET,
+        });
+        const data = response;
+        const token = " ";
 
-    useEffect(()=>{
-        getSkills();
-        getUserSkills();
-        if(userType == 3){
-
-            setInputs({user_name : data.user_name,description : data.rec_details.description, company_name : data.rec_details.company_name});
-        }else{
-            setInputs({
-                user_name : data.user_name,
-                description : data.dev_details && data.dev_details.description? data.dev_details.description : "no description yet",
-                gender : data.dev_details && data.dev_details.gender ?data.dev_details.gender : "gender",
-                github_url : data.dev_details && data.dev_details.github_url ?data.dev_details.github_url : "add your github",
-                linkedin_url : data.dev_details && data.dev_details.linkedin_url ?data.dev_details.linkedin_url : "add your linkedin",
-            });
+        if (data.status == "success") {
+          const obj = data.data;
+          setSkills(obj);
+        } else {
+          setError("failed to get user data!");
+          console.log(error);
         }
-    },[search, userType]);
-
-    const inputChange = (event) => {
-        setSearch(event.target.value);
-        getSkills()
-    };
-
-    const onChangeHandler = id => () => {
-      selected.includes(id)
-        ? setSelected(selected.filter(x => x !== id))
-        : setSelected([...selected, id]);
-    };
-        
-    const filtering = (id) =>{
-        const filtered = userSkills.filter(row => row.skill.id !== id)
-        setUserSkills(filtered)
+      }
+    } catch (error) {
+      console.error("failed to get user:", error);
     }
+  };
 
-    const hideModel =() =>{
-        isOpen(prev => !prev);
-    }
+  const getUserSkills = async () => {
+    const token = localStorageAction("token");
+    const userId = localStorageAction("user_id");
 
-    const getSkills = async () => {
-        const token = localStorageAction("token");
+    try {
+      if (!token) {
+        setError("there is nothing to show here");
+        console.log(error);
+      } else {
+        const response = await sendRequest({
+          route: `/user/developer/view_user_skills/${userId}`,
+          method: requestMethods.GET,
+        });
+        const data = response;
+        const token = " ";
 
-        try {
-            if(!token){
-                setError('there is nothing to show here');
-                console.log(error);
-            }else{
-
-                const response = await sendRequest({
-                    route: `/user/developer/view_all_skills/${search}`,
-                    method: requestMethods.GET,
-                });
-                const data = response;
-                const token = " ";
-    
-                if(data.status == 'success'){
-                    const obj = data.data;
-                    setSkills(obj);
-
-                }else{
-                    setError("failed to get user data!");
-                    console.log(error);
-                }
-            }
-            
-          } catch (error) {
-            console.error("failed to get user:", error);
-          }
-    }
-
-    const getUserSkills = async () =>{
-        const token = localStorageAction("token");
-        const userId = localStorageAction("user_id");
-
-        try {
-            if(!token){
-                setError('there is nothing to show here');
-                console.log(error);
-            }else{
-
-                const response = await sendRequest({
-                    route: `/user/developer/view_user_skills/${userId}`,
-                    method: requestMethods.GET,
-                });
-                const data = response;
-                const token = " ";
-    
-                if(data.status == 'success'){
-                    const obj = data.data;
-                    setUserSkills(obj);
-
-                }else{
-                    setError("no skills exist!");
-                    console.log(error);
-                }
-            }
-            
-          } catch (error) {
-            console.error("Fetching skills failed:", error);
-          }
-    }
-
-    const addUserSkills = async () =>{
-        const mySkills = JSON.stringify(selected);
-
-        try {
-            if(!selected){
-                setError('no skills to add');
-                console.log(error);
-            }else{
-
-                const response = await sendRequest({
-                    route: "/user/developer/add_skills",
-                    method: requestMethods.POST,
-                    body:{user_skills: mySkills}
-                });
-                const data = response;
-                const token = " ";
-    
-                if(data.status == 'success'){
-                    console.log("successfully add")
-                }else{
-                    setError("failed to add!");
-                    console.log(error);
-                }
-            }
-            
-          } catch (error) {
-            console.error("bad request. failed:", error);
-          }
-    }
-
-    const removeUserSkills = async (id) =>{
-        const removeSkill = JSON.stringify([id]);
-
-        try {
-            const response = await sendRequest({
-                route: "/user/developer/remove_skills",
-                method: requestMethods.POST,
-                body:{user_skills: removeSkill}
-            });
-            const data = response;
-            const token = " ";
-
-            if(data.status == 'successfully deleted skills'){
-                filtering(id)
-            }else{
-                setError("failed to add!");
-                console.log(error);
-            }
-            
-          } catch (error) {
-            console.error("bad request. failed:", error);
-          }
-    }
-
-    const updateUserInfo = async () =>{
-
-        try {
-            if(!inputs){
-                setError('nothing to change');
-                console.log(error);
-            }else{
-
-                const response = await sendRequest({
-                    route: "user/developer/update-details",
-                    method: requestMethods.POST,
-                    body:{user_name: inputs.user_name,
-                        description: inputs.description,
-                        gender: inputs.gender,
-                        company_name: inputs.company_name,
-                        github_url: inputs.github_url,
-                        linkedin_url: inputs.linkedin_url,
-                    }
-                });
-                const data = response;
-                const token = " ";
-    
-                if(data.status == 'success'){
-                    console.log("successfully updated")
-                    hideModel()
-                }else{
-                    setError("failed to update!");
-                    console.log(error);
-                }
-            }
-            
-          } catch (error) {
-            console.error("bad request. failed:", error);
-          }
-    }
-    console.log("my user skills are", userSkills)
-
-    const save = async () =>{
-        try{
-            await updateUserInfo();
-            await addUserSkills();
-        }catch (error){
-            console.log('faled to save', error);
+        if (data.status == "success") {
+          const obj = data.data;
+          setUserSkills(obj);
+          const initialSelected = obj.map((userSkill) => userSkill.skill.id);
+          setSelected(initialSelected);
+        } else {
+          setError("no skills exist!");
+          console.log(error);
         }
+      }
+    } catch (error) {
+      console.error("Fetching skills failed:", error);
     }
+  };
+
+  const addUserSkills = async () => {
+    const mySkills = JSON.stringify(selected);
+
+    try {
+      if (!selected) {
+        setError("no skills to add");
+        console.log(error);
+      } else {
+        const response = await sendRequest({
+          route: "/user/developer/add_skills",
+          method: requestMethods.POST,
+          body: { user_skills: mySkills },
+        });
+        const data = response;
+        const token = " ";
+
+        if (data.status == "success") {
+          console.log("successfully added");
+        } else {
+          setError("failed to add!");
+          console.log(error);
+        }
+      }
+    } catch (error) {
+      console.error("bad request. failed:", error);
+    }
+  };
+
+  const removeUserSkills = async () => {
+    const removeSkill =  JSON.stringify(unselectedSkills);
+    console.log('my skills to be removeed arteeeeeee', removeSkill)
+
+    try {
+      const response = await sendRequest({
+        route: "/user/developer/remove_skills",
+        method: requestMethods.POST,
+        body: { user_skills: removeSkill },
+      });
+      const data = response;
+      const token = " ";
+
+      if (data.status == "success") {
+        setUserSkills((prevSkills) =>
+        prevSkills.filter((row) => !unselectedSkills.includes(row.skill.id))
+      );
+      } else {
+        setError("failed to remove!");
+        console.log(error);
+      }
+    } catch (error) {
+      console.error("bad request. failed:", error);
+    }
+  };
+
+  const updateUserInfo = async () => {
+    try {
+      if (!inputs) {
+        setError("nothing to change");
+        console.log(error);
+      } else {
+        const response = await sendRequest({
+          route: "user/developer/update-details",
+          method: requestMethods.POST,
+          body: {
+            user_name: inputs.user_name,
+            description: inputs.description,
+            gender: inputs.gender,
+            company_name: inputs.company_name,
+            github_url: inputs.github_url,
+            linkedin_url: inputs.linkedin_url,
+          },
+        });
+        const data = response;
+        const token = " ";
+
+        if (data.status == "success") {
+          console.log("successfully updated");
+          hideModel();
+        } else {
+          setError("failed to update!");
+          console.log(error);
+        }
+      }
+    } catch (error) {
+      console.error("bad request. failed:", error);
+    }
+  };
+
+  const save = async () => {
+    try {
+      await updateUserInfo();
+      await addUserSkills();
+      await removeUserSkills();
+      hideModel()
+      window.location.href = `/dashboard/profile/${userId}`
+    } catch (error) {
+      console.log("failed to save", error);
+    }
+  };
 
     return(
         <div className={styles.popup_container}>
@@ -295,16 +285,6 @@ const EditForm = ({isOpen, data}) =>{
                 
                 <div className={styles.skill_container}>
                     <div className={styles.header}>Skills</div>
-                    <div className={styles.scrollable_container}>
-                    {userSkills && userSkills.map((skill)=>(
-                        <CustomImageButton key={skill.skill_id} text={`${skill.skill.name}`} image_name={'Close.png'} display={'flex'} flexDirection={'row-reverse'} alignItems={'center'} backgroundColor={'#C2D0FF'} padding={'0.6rem 1rem'} borderRadius={8} image_height={18} image_width={18} width={'fit-content'} cursor={'pointer'} onClick={()=> removeUserSkills(skill.skill.id)}/>
-                    ))}
-                    {!userSkills ?(
-                        <div>Starting adding skills to you profile</div>
-                    ):(
-                        <div></div>
-                    )}
-                    </div>
                 </div>
                 <div className={styles.searchable}>
                     <div className={styles.top_bar}>
