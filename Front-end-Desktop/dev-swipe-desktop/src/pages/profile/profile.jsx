ea820e33b7592c60f76aac78d611e68e1147452c
User <majedhabli2020@gmail.com>
2023-10-01 21:56:23 +0300

    Client-Desktop <> Cleaned profile page.

--- Front-end-Desktop/dev-swipe-desktop/src/pages/profile/profile.jsx
+++ Front-end-Desktop/dev-swipe-desktop/src/pages/profile/profile.jsx
@@ -165,8 +165,6 @@ const Profile = () =>{
         getImages();
     },[params.id]);
 
-    console.log(user)
-
     useEffect(()=>{
         setStats({liked_count: stats && stats.liked_count ? stats.liked_count: '0',matched_count: stats && stats.matched_count ? stats.matched_count: '0',skipped_count: stats && stats.skipped_count ? stats.skipped_count: '0',view_count: stats && stats.view_count ? stats.view_count: '0'});
         getStats();
@@ -219,29 +217,3 @@ const Profile = () =>{
 }
 
 export default Profile;
-
-{/* <div className={styles.page_header}></div>
-            <div className={styles.header_comp}>
-                <div className={styles.head}>
-                    <HeaderComp data={user}/>
-                </div>
-                <div className={styles.button_container}>
-                    <CustomImageButton text={'upload new resume'} width={190} height={34} display={'flex'} alignItems={'center'} columnGap={'1rem'} image_name={"Vector.png"} image_height={16} image_width={16} backgroundColor={'white'} padding={'0 .5rem'} borderRadius={4} boxShadow={'0 2px 16px 0 rgba(0, 0, 0, 0.1), 0 2px 8px 0 rgba(0, 0, 0, 0.1)'}/>
-                </div>
-            </div>
-            <div className={styles.component_container}>
-                <div className={styles.componenet_body}>
-                    <div className={styles.left_container}>
-                        <div className={styles.title}>Skills: </div>
-                        <div className={styles.skill_cotainer}>
-                            {skills.map((skill)=>(
-                                <CustomImageButton key={skill.skill_id} text={`${skill.skill.name}`} width={213} height={56} display={'flex'} alignItems={'center'} columnGap={'1rem'} backgroundColor={'#FCC860'} padding={'0.5rem .5rem'} borderRadius={4} boxShadow={'0 2px 16px 0 rgba(0, 0, 0, 0.1), 0 2px 8px 0 rgba(0, 0, 0, 0.1)'}/>
-                            ))}
-                            {errorSkills &&(<div className={styles.error_container}>{errorSkills}</div>)}
-                        </div>
-                    </div>
-                    <div className={styles.right_container}>
-                        <CarouselComp value={images} issue={errorImages}/>
-                    </div>
-                </div>
-            </div> */}
